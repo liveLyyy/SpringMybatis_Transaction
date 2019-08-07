94f8acf4d1cc468a49a8aa59969f28168c2d59c1
liveLyyy <863745650@qq.com>
2019-08-07 17:45:08 +0800

    1.1

---
+++ src/main/java/com/liyan/service/Impl/UsersServiceImpl.java
@@ -0,0 +1,11 @@
+package com.liyan.service.Impl;
+
+import com.liyan.pojo.Users;
+import com.liyan.service.UsersService;
+
+public class UsersServiceImpl implements UsersService {
+    @Override
+    public int insert(Users users) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/liyan/service/UsersService.java
@@ -0,0 +1,7 @@
+package com.liyan.service;
+
+import com.liyan.pojo.Users;
+
+public interface UsersService {
+    int insert(Users users);
+}
